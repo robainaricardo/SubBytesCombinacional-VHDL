9a9e8871bcc07858ce5913f18ad38b867a9ab828
Ricardo P. Robaina <ricardorobaina@outlook.com>
2017-10-04 12:38:04 -0300

    Entradas ligadas
    Processo quase pronto, dado erro no shift.

---
+++ SubBytesLowPowerVerilog/work/@sub@bytes@l@p/_primary.vhd
@@ -0,0 +1,11 @@
+library verilog;
+use verilog.vl_types.all;
+entity SubBytesLP is
+    port(
+        clock           : in     vl_logic;
+        reset           : in     vl_logic;
+        enable          : in     vl_logic;
+        blocoIn         : in     vl_logic_vector(0 to 127);
+        blocoOut        : out    vl_logic_vector(0 to 127)
+    );
+end SubBytesLP;
